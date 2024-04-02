b4067ced8c39c8294d6385477deb286e254f8dbf
shanetoken <158006619+shanetoken@users.noreply.github.com>
2024-04-02 11:43:21 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="card">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/counter/Counter.jsx
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './counterSlice';
+
+export function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="card">
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+    </div>
+  );
+}
